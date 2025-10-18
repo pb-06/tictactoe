b49e5cf49482b5ab95793ad7bbb853262cbd5dea
pb-06 <kkandomail@gmail.com>
2025-10-18 13:05:03 +0200

    Add task files

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TicTacToe from './TicTacToe';
 
-function App() {
-  const [count, setCount] = useState(0)
+/**
+ * @task TODO - track version control with Git.
+ * Branching logic:
+ * production: prod (= renamed or created from main or master)
+ * development: dev
+ * - common feature branch: feature/tictactoe
+ * - individual dev branch: feature/tictactoe/SajatNev
+ * Merge logic:
+ * feature/tictactoe/SajatNev -> feature/tictactoe -> dev -> prod
+ */
 
+function App() { 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <TicTacToe />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </> 
   )
 }
 
---
+++ src/TicTacToe.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+// TODO - npm install express cors react-toastify
+//import { toast, ToastContainer } from 'react-toastify';
+//import 'react-toastify/dist/ReactToastify.css';
+import './App.css'; // with custom childish styles
+
+function TicTacToe() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [nextPlayer, setNextPlayer] = useState('X');
+  const [winner, setWinner] = useState(null);
+
+  const postResult = async (result) => {
+    // TODO - implement backend POST
+    await fetch('/api/result', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ result })
+    });
+  };
+  // TODO - implement backend GET: query previous match results
+
+  const calculateWinner = function(board) {/* TODO - implement */}
+
+  const handleClick = (idx) => {
+    //TODO - if (board[idx] || winner) return;
+    const newBoard = board.slice();
+    newBoard[idx] = 'X'; // TODO
+    
+    setBoard(newBoard);
+    setNextPlayer('O'); // TODO
+    const win = calculateWinner(newBoard);
+    if (win) {
+      setWinner(win);
+      //toast(`${win} wins!`);
+      //postResult(`${win} wins`);
+    } else if (!newBoard.includes(null)) {
+      //toast('Draw!');
+      //postResult('Draw');
+    }
+  };
+
+  const handleRestart = () => {/* TODO */};
+
+  const handleSurrender = () => {
+    if (!winner) {/* TODO */}
+  };
+
+return (
+  <div className="gameContainer">
+    <h1>TicTacToe!</h1>
+    <div className="grid">
+      {
+        [0,1,2].map(row => (
+          <div className="board-row" key={row}>
+            {[0,1,2].map(col => {
+              const idx = row * col // TODO - recalculate button address index
+              return (
+                <button 
+                  key="idx" 
+                  className="square" 
+                  
+                >
+                  {board?.idx} {/* TODO */}
+                </button>
+              );
+            })}
+          </div>
+        ))
+      }
+    </div>
+    <div className="controls">
+      <button >Restart</button>
+      <button >Surrender</button>
+    </div>
+    {/*<ToastContainer position="top-center" />*/}
+  </div>
+);
+}
+
+export default TicTacToe;
