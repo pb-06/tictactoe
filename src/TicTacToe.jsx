f4d6317d0a0d82b1eed75e450f6e2f3a0ffe1a79
pb-06 <kkandomail@gmail.com>
2025-10-18 13:34:25 +0200

    Create calculateWinner()

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -20,7 +20,25 @@ function TicTacToe() {
   };
   // TODO - implement backend GET: query previous match results
 
-  const calculateWinner = function (board) {/* TODO - implement */ }
+  const calculateWinner = (currentBoard) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (currentBoard[a] && currentBoard[a] === currentBoard[b] && currentBoard[a] === currentBoard[c]) {
+        return currentBoard[a];
+      }
+    }
+    return null;
+  };
 
   const handleClick = (idx) => {
     if (board[idx] || winner) {
